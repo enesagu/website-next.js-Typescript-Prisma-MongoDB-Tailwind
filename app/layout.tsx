20c69e6d75d6b6401625a31f3d703e528bdf68a0
enesagu <97469455+enesagu@users.noreply.github.com>
2024-09-22 18:07:06 +0300

    ico change.
    Just for viewing

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+      <div className="flex flex-col min-h-screen">
+              <Navbar />
 
-        {children}
+       <main className="flex-grow"> {children}</main>
         <Footer />
+      </div>
       </body>
     </html>
   );
