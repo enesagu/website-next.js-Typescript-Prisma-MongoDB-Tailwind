5b738e38521a2e9f8e77378f34a5170a6445f9de
enesagu <97469455+enesagu@users.noreply.github.com>
2024-09-24 05:24:52 +0300

    Update ProductCard
    update for product name was hiding picture. It was behind the <div/>

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -2,22 +2,20 @@ import Image from "next/image";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <div className="w-[240px] shadow-lg p-2 rounded-md">
-      <div className="relative h-[150px]">
+    <div className="w-[240px] shadow-lg p-2 rounded-md ">
+      <div className="relative h-[150px] ">
         {product?.image ? (
-          <Image
-            src={product.image} alt={""} width={240} height={75} className="object-contain w-[120px]"
-          />
+          <Image src={product.image} fill alt={""} className="object-contain" />
         ) : (
-          <div className="bg-gray-200 h-[150px] w-full flex items-center justify-center">
+          <div className="bg-gray-200 h-[200px] w-full flex items-center justify-center">
             <p>No Image Available</p>
           </div>
         )}
-      </div>
 
-      {/* Ürünün ismi */}
+        {/* Ürünün ismi varsa göster */}
+      </div>
       <div className="text-center mt-2">
-        <p>{product?.name || "No Product Name"}</p>
+        <div>{product?.name || "No Product Name"}</div>
       </div>
     </div>
   );
